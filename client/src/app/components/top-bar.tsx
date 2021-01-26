afc41bfe97b5367d5e315322819e08712560559a
Dominik Domanski <dominik.d@syte-vc.com>
2021-01-26 15:53:30 +0200

    more styling

--- client/src/app/components/top-bar.tsx
+++ client/src/app/components/top-bar.tsx
@@ -4,7 +4,6 @@ export const TopBar = () => {
     return (
         <>
             <div className="topbar">
-                <div className="topbar-dropdown"/>
                 <div className="topbar-title">Vehicles: O'Coffee Brazil</div>
                 <div className="topbar-logo" />
             </div>
